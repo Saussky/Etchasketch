3379058a7261c57e6dbc2fda5e3d3c022e824158
Saucy <patrickgardnerbrunton@hotmail.com>
2022-06-21 17:18:52 +1000

    added a make your own grid button

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 // access the container element from the HTML file
 const container = document.querySelector('#container');
 
+
 // creating 16 divs 
-for (i = 1; i <= 16; i++){
+function createDivs(n) {
+    for (i = 1; i <= n; i++){
     const newDiv = document.createElement('div');
     newDiv.classList.add(`box`);
     newDiv.innerText = `${i}`;
     container.appendChild(newDiv);
-
+    }
 }
+createDivs(16);
 
-// chaning colour of each Div if it is hovered over
 const sections = document.querySelectorAll('.box');
 console.log(sections);
 
+// Every div will wait for the mouse to hover over it, and if it does it will turn red
 sections.forEach((div) => {
     div.addEventListener("mouseenter",  function (e) {
         capture: false;
         e.stopPropagation;
         div.classList.add('coloured');
-        
-
     });
 });
 
+
+// on button click come up with a prompt
+
+
+document.querySelector('button').onclick = function getInput() {
+    let x = prompt("How many blocks");
+    console.log(x);
+
+    deletThis = document.querySelectorAll('.box');
+    deletThis.forEach(grid => {
+        grid.remove();
+    });
+
+    createDivs(x);
+}
