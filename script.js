3cb2780e1a3b110c4caaa5c59023d8661afdd7e1
Saucy <patrickgardnerbrunton@hotmail.com>
2022-06-21 17:04:33 +1000

    added mousehover changes color

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+// access the container element from the HTML file
 const container = document.querySelector('#container');
 
+// creating 16 divs 
 for (i = 1; i <= 16; i++){
     const newDiv = document.createElement('div');
+    newDiv.classList.add(`box`);
     newDiv.innerText = `${i}`;
     container.appendChild(newDiv);
+
 }
+
+// chaning colour of each Div if it is hovered over
+const sections = document.querySelectorAll('.box');
+console.log(sections);
+
+sections.forEach((div) => {
+    div.addEventListener("mouseenter",  function (e) {
+        capture: false;
+        e.stopPropagation;
+        div.classList.add('coloured');
+        
+
+    });
+});
+
